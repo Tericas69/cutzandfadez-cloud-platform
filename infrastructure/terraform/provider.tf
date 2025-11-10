63a5ee4ebf95caf76fef935ce482d7bc633c4890
Terica Shepard <therod1@collin.edu>
2025-11-09 20:09:03 -0600

    Added provider.tf for AWS provider configuration

---
+++ infrastructure/terraform/provider.tf
@@ -0,0 +1,18 @@
+# provider.tf
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  # Uncomment and replace "default" if you use a specific AWS CLI profile:
+  # profile = "default"
+}
