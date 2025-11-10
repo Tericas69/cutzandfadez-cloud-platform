3db8e25c73d37388bbdbe30925ca2abd21370534
Terica Shepard <therod1@collin.edu>
2025-11-09 20:21:32 -0600

    Added Terraform outputs for VPC, S3, and RDS resources

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,12 +1,37 @@
 # outputs.tf
-# Display key information after Terraform deployment
+# Display key resource details after apply
 
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket for static site hosting"
-  value       = aws_s3_bucket.static_site.bucket
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
 }
 
-output "region" {
-  description = "The AWS region used for deployment"
-  value       = var.aws_region
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket hosting the frontend"
+  value       = aws_s3_bucket.frontend_bucket.bucket
+}
+
+output "s3_bucket_website_endpoint" {
+  description = "Website endpoint for the S3 bucket"
+  value       = aws_s3_bucket.frontend_bucket.website_endpoint
+}
+
+output "rds_endpoint" {
+  description = "RDS database endpoint"
+  value       = aws_db_instance.app_db.endpoint
+}
+
+output "rds_db_name" {
+  description = "RDS database name"
+  value       = aws_db_instance.app_db.name
 }
